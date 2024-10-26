0ee95e480781b5371e96241d31ad4dd8668d48b2
suzannefox <suzanne_fox@hotmail.com>
2024-10-26 21:17:50 +0100

    COMMIT : Basic Streamlist app

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# .\venv\Scripts\activate
+
+
+
+st.title("Exploring Saturn")
+st.write("This is a simple Streamlit app to learn about the planet Saturn.")
+
+st.image("https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg",
+         caption="Saturn", use_column_width=True)
+st.markdown("""
+## Facts about Saturn
+- It is the sixth planet from the Sun.
+- Saturn has a ring system composed mainly of ice particles and rocky debris.
+- It is a gas giant, like Jupiter.
+""")
